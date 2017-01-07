af80468362f2032a5117f39d185a5b17f12e6ee8
Alexander Polakov <plhk@sdf.org>
2017-01-07 22:03:36 +0300

    Can't define my own packets?

---
+++ build.rs
@@ -0,0 +1,33 @@
+extern crate syntex;
+extern crate pnet_macros;
+
+use std::env;
+use std::path::Path;
+use std::fs;
+
+const FILES: &'static [&'static str] = &[
+    "test.rs",
+];
+
+pub fn expand() {
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+
+    for file in FILES {
+        let src_file = format!("src/packet/{}.in", file);
+        let src = Path::new(&src_file);
+        let dst_name = Path::new(file);
+        if let Some(parent) = dst_name.parent() {
+            fs::create_dir(Path::new(&out_dir).join(parent));
+        }
+        let dst = Path::new(&out_dir).join(dst_name);
+
+        let mut registry = syntex::Registry::new();
+        pnet_macros::register(&mut registry);
+
+        registry.expand("", &src, &dst).unwrap();
+    }
+}
+
+fn main() {
+    expand();
+}
---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+extern crate pnet;
+
+mod packet;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
